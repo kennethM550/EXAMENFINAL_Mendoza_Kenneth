7ba9d29151ea97296ccd0937af4abd5346641c28
kennethM550 <kenneth.mendoza@unmsm.edu.pe>
2022-01-26 20:28:30 -0500

    País y País arreglo

--- src/modelo/PaisArreglo.java
+++ src/modelo/PaisArreglo.java
@@ -3,5 +3,39 @@ package modelo;
 
 
 public class PaisArreglo {
+    private int indice;
+    private Pais pais[];
     
+    public PaisArreglo(int tamaño){
+        this.indice = 0;
+        this.pais = new Pais[tamaño];
+    }
+    
+    public boolean agregar(Pais pais){
+        boolean result = false;
+        this.pais[this.indice]=pais;
+        this.indice++;
+        result = true;
+        return result;
+    }
+    
+    public boolean eliminar(Pais pais){
+        boolean result = false;
+        this.pais[this.indice]=pais;
+        this.indice--;
+        result = true;
+        return result;
+    }
+    
+    public Pais getPais(String codigo){
+        Pais result = null;
+        for (int i = 0; i < this.pais.length; i++) {
+            if (this.pais[i].getCodigo().equals(codigo)) {
+                
+                result=this.pais[i];
+            }
+          }
+        return result;
+    }
 }
+
